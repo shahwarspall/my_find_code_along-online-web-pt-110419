a05bbb03767be82325943c1362aa44889ae64254
Shahwar Spall <shevy.spall@gmail.com>
2019-11-14 04:55:34 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,9 +4,6 @@ def my_find(collection)
   i = 0 
   block_return =[]
   while i < collection.length 
-  block_return << yield(collection[i])
+  return collection[i] if yield(collection[i])
   i = i + 1 
-end
-if 
-  block_return.myfind?(1)
 end
